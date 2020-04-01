13e3665ca41300b808f682fbc67371e03fa46351
Michael <ko4erov@mail.ru>
2020-04-01 17:50:11 +0300

    add font and header list

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import './../blocks/good-card/good-card.js'
+import './../blocks/main-slider/main-slider.js'
 import './../blocks/footer-subscribe/footer-subscribe.js'
 import './../blocks/footer-contacts/footer-contacts.js'
 import './../blocks/footer-socials/footer-socials.js'
