1471253aafe7af1c70e52c46b3a3ec13853fb7fb
Michael <ko4erov@mail.ru>
2020-04-01 12:02:03 +0300

    add footer files

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,4 @@
 import './../blocks/good-card/good-card.js'
+import './../blocks/footer-subscribe/footer-subscribe.js'
+import './../blocks/footer-contacts/footer-contacts.js'
+import './../blocks/footer-socials/footer-socials.js'
