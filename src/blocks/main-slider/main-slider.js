e83d21e1325e9a568bc3d53c0cb7538301061045
Michael <ko4erov@mail.ru>
2020-04-01 20:36:26 +0300

    create mainSlider script and styles

--- src/blocks/main-slider/main-slider.js
+++ src/blocks/main-slider/main-slider.js
@@ -1,1 +1,53 @@
 import './main-slider.scss'
+
+mainSlider ('[data-slider]')
+
+// для корректной работы после последнего элемента вставить копию первого
+// {data-slider: {data-arrow-left, data-arrow-right, data-slider-wrapper:[data-slider-item, data-slider-item, ...]}}
+function mainSlider (attr) {
+    const sliderList = document.querySelectorAll(attr)
+
+    sliderList.forEach((slider) => {
+        const leftArrowElement = slider.querySelector('[data-arrow-left]')
+        const rightArrowElement = slider.querySelector('[data-arrow-right]')
+        const slidersWrapperElement = slider.querySelector('[data-slider-wrapper]')
+        // считаем количество активных слайдов
+        const count = slider.querySelectorAll('[data-slider-item]').length - 1
+        // позиция обёртки
+        let translatePosition = 0
+
+        // стрелка влево
+        leftArrowElement.addEventListener('click', () => {
+            if (translatePosition > -100 * count) {
+                slidersWrapperElement.style = `transform: translateX(${translatePosition -= 100}%);`
+                
+                if (translatePosition === -100 * count) {
+                    setTimeout(() => {
+                        slidersWrapperElement.style = `transition: none;`
+                        setTimeout (() => {
+    
+                            slidersWrapperElement.style += `transform: translateX(${translatePosition = 0}%);`
+                        }, 100)
+
+                    }, 400)
+                }
+            }
+        })
+
+        // стрелка вправо
+        rightArrowElement.addEventListener('click', () => {
+            
+            if (translatePosition < 0) {
+                slidersWrapperElement.style = `transform: translateX(${translatePosition += 100}%);`
+            }
+            else if (translatePosition === 0){
+
+                slidersWrapperElement.style = `transition: none; transform: translateX(${translatePosition = -100 * count}%);`
+                setTimeout(() => {
+                    slidersWrapperElement.style = `transform: translateX(${translatePosition += 100}%);`
+                },10)
+
+            }
+        })
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+// import owlCarousel from "owl.carousel";
 // SCSS
 import './assets/scss/main.scss'
 
 // JS
 import './js/index.js'
+// $(document).ready(function(){
+// $('.owl-carousel').owlCarousel();
+// });
