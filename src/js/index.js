9fee7d559517b62639afe8dff22ffa0fcf17f332
Michael <ko4erov@mail.ru>
2020-04-02 10:28:48 +0300

    subscribe ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import './../blocks/good-card/good-card.js'
 import './../blocks/main-slider/main-slider.js'
 import './../blocks/banner/banner.js'
+import './../blocks/subscribe/subscribe.js'
 import './../blocks/footer-subscribe/footer-subscribe.js'
 import './../blocks/footer-contacts/footer-contacts.js'
 import './../blocks/footer-socials/footer-socials.js'
