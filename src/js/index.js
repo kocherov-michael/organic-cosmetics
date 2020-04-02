db1a495418d25f5375d4d954ed53fe9e51815b7e
Michael <ko4erov@mail.ru>
2020-04-02 09:44:09 +0300

    banner ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import './../blocks/good-card/good-card.js'
 import './../blocks/main-slider/main-slider.js'
+import './../blocks/banner/banner.js'
 import './../blocks/footer-subscribe/footer-subscribe.js'
 import './../blocks/footer-contacts/footer-contacts.js'
 import './../blocks/footer-socials/footer-socials.js'
