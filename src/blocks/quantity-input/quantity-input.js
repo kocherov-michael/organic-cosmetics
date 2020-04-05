d26dfad224dbcf81e97a8becf3dfdab9acd4758d
Michael <ko4erov@mail.ru>
2020-04-05 14:07:06 +0300

    quantity-input ready

--- src/blocks/quantity-input/quantity-input.js
+++ src/blocks/quantity-input/quantity-input.js
@@ -1,1 +1,31 @@
 import './quantity-input.scss'
+
+quantityInput()
+
+function quantityInput() {
+    const inputList = document.querySelectorAll('[data-quantity-input]')
+    for (let i = 0; i < inputList.length; i++) {
+        const valueElement = inputList[i].querySelector('[data-input-value]')
+        const plusElement = inputList[i].querySelector('[data-input-plus]')
+        const minusElement = inputList[i].querySelector('[data-input-minus]')
+
+        plusElement.addEventListener('click', () => {
+            valueElement.value = +valueElement.value + 1
+            valueElement.focus()
+        })
+        minusElement.addEventListener('click', () => {
+            if (valueElement.value > 0) {
+                valueElement.value = +valueElement.value - 1
+            }
+            valueElement.focus()
+        })
+        valueElement.addEventListener('focus', () => {
+            inputList[i].classList.add('quantity-input--shadow')
+        })
+        valueElement.addEventListener('blur', () => {
+            inputList[i].classList.remove('quantity-input--shadow')
+        })
+
+
+    }
+}
