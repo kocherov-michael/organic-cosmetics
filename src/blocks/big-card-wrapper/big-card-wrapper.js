f8f0d36b9b3465dfe2bd10f139d4f177751069a4
Michael <ko4erov@mail.ru>
2020-04-07 18:45:19 +0300

    cart item html ready

--- src/blocks/big-card-wrapper/big-card-wrapper.js
+++ src/blocks/big-card-wrapper/big-card-wrapper.js
@@ -5,42 +5,45 @@ showBigCard()
 
 function showBigCard() {
     const goodCardButtonsList = document.querySelectorAll('[data-card-look]')
-    const wrapperElement = document.querySelector('[data-big-card-wrapper]')
-    const closeElement = document.querySelector('[data-card-close]')
-    // console.log(wrapperElement)
+    if (goodCardButtonsList.length > 0) {
 
-    for(let i = 0; i < goodCardButtonsList.length; i++) {
-
-        goodCardButtonsList[i].addEventListener('click', () => {
-            // console.log(goodCardButtonsList[i])
-            const href = getHref(goodCardButtonsList[i])
-            const obj = splitUrl(href)
-            // console.log(obj)
-            // заполняем попап карточку
-            fillCard(obj)
-
-            wrapperElement.classList.add('big-card-wrapper--translate')
+        const wrapperElement = document.querySelector('[data-big-card-wrapper]')
+        const closeElement = document.querySelector('[data-card-close]')
+        // console.log(wrapperElement)
+    
+        for(let i = 0; i < goodCardButtonsList.length; i++) {
+    
+            goodCardButtonsList[i].addEventListener('click', () => {
+                // console.log(goodCardButtonsList[i])
+                const href = getHref(goodCardButtonsList[i])
+                const obj = splitUrl(href)
+                // console.log(obj)
+                // заполняем попап карточку
+                fillCard(obj)
+    
+                wrapperElement.classList.add('big-card-wrapper--translate')
+                setTimeout(() => {
+                    wrapperElement.classList.add('big-card-wrapper--transition-none')
+                    wrapperElement.style = `top: ${pageYOffset}px;`
+                    document.body.style = 'overflow: hidden; padding-right: 18px;'
+                    wrapperElement.classList.add('big-card-wrapper--show')
+                },400)
+            })
+        }
+    
+        closeElement.addEventListener('click', () => {
+            document.body.style = ''
+            wrapperElement.style = ''
+            wrapperElement.classList.remove('big-card-wrapper--show')
             setTimeout(() => {
-                wrapperElement.classList.add('big-card-wrapper--transition-none')
-                wrapperElement.style = `top: ${pageYOffset}px;`
-                document.body.style = 'overflow: hidden; padding-right: 18px;'
-                wrapperElement.classList.add('big-card-wrapper--show')
-            },400)
+                wrapperElement.classList.remove('big-card-wrapper--transition-none')
+                setTimeout(() => {
+                    wrapperElement.classList.remove('big-card-wrapper--translate')
+    
+                },40)
+            },40)
         })
     }
-
-    closeElement.addEventListener('click', () => {
-        document.body.style = ''
-        wrapperElement.style = ''
-        wrapperElement.classList.remove('big-card-wrapper--show')
-        setTimeout(() => {
-            wrapperElement.classList.remove('big-card-wrapper--transition-none')
-            setTimeout(() => {
-                wrapperElement.classList.remove('big-card-wrapper--translate')
-
-            },40)
-        },40)
-    })
 }
 
 function getHref(elem) {
