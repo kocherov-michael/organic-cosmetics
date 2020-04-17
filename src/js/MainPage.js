e2d1f70c1f949b88bcc7f1cdd4aa748e7077e3f6
Michael <ko4erov@mail.ru>
2020-04-17 10:41:05 +0300

    refactoring goods card

--- src/js/MainPage.js
+++ src/js/MainPage.js
@@ -4,10 +4,7 @@ import DefaultPage from './DefaultPage.js'
 export default class MainPage extends DefaultPage {
     constructor(args = {}) {
         super(args = {})
-        // this.goodsArr = goodsArr[0]
-        // this.cart = JSON.parse(localStorage.getItem('cart')) || []
-        // super.showCartLength()
-        // super.fillCartCard()
+        // пока отключил
         this.fillMainPage()
         super.listenAddCartButton()
         super.quantityInput()
@@ -25,31 +22,44 @@ export default class MainPage extends DefaultPage {
         for ( let i = 0; i < 12; i++ ) {
             
             innerElement += 
-            `<div class="goods__card col-6 col-md-4 col-lg-3">
-            <div class="good-card" data-goods-card="">
-              <div class="good-card__header">
-                <div class="good-card__picture"><img class="good-card__img" src="./assets/img/goods/${arr[i].src}" alt="Foaming Hand Sanitizer"></div>
-                <div class="good-card__billets">
-                  <div class="good-card__billet-wrapper">
-                    <div class="billet billet--sale">on sale</div>
+            `<div class="goods__card col-sm-6 col-md-4 col-lg-3">
+              <div class="good-card" data-goods-card="">
+                <div class="good-card__header">
+                  <div class="good-card__picture"><img class="good-card__img" src="./assets/img/goods/${arr[i].src}" alt="Container is Perfect for Essential Oils"></div>
+                  <div class="good-card__billets">
+                    <div class="good-card__billet-wrapper">
+                      <div class="billet billet--sale">on sale</div>
+                    </div>
+                    <div class="good-card__billet-wrapper">
+                      <div class="billet billet--new">new</div>
+                    </div>
                   </div>
-                  <div class="good-card__billet-wrapper">
-                    <div class="billet billet--new">new</div>
+                  <div class="good-card__look">
+                    <div class="good-card__look-button" data-card-look="${arr[i].id}">быстрый просмотр</div>
                   </div>
                 </div>
-                <div class="good-card__look">
-                  <div class="good-card__look-button" data-card-look="${arr[i].id}">быстрый просмотр</div>
+                <div class="good-card__main">
+                  <div class="good-card__oferta-wrapper">
+                  </div>
+                  <div class="good-card__group">${arr[i].subcategory}</div>
+                  <a class="good-card__title" data-goods-link="" href="product.html?id=${arr[i].id}">${arr[i].name}</a>
+                  <div class="good-card__old-price-wrapper">
+                  </div>
+                </div>
+                <div class="good-card__footer">
+                  <div class="good-card__footer-price">
+                    <div class="good-card__price">${arr[i].price}</div>
+                  </div>
+                  <div class="good-card__footer-icons">
+                    <div class="good-card__footer-icon-item">
+                      <div class="zmdi zmdi-eye"></div>
+                    </div>
+                    <div class="good-card__footer-icon-item">
+                      <div class="zmdi zmdi-shopping-cart"></div>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div class="good-card__main">
-                <div class="good-card__oferta-wrapper">
-                  <div class="good-card__oferta">-20% </div>
-                </div><a class="good-card__group" href="category.html">${arr[i].subcategory}</a><a class="good-card__title" data-goods-link="" href="product.html?id=${arr[i].id}">${arr[i].name}</a>
-                <div class="good-card__price">${arr[i].price} $</div>
-                <div class="good-card__old-price">16.00 $</div>
-              </div>
-              <div class="good-card__footer"></div>
-            </div>
             </div>`
         }
 
@@ -57,8 +67,4 @@ export default class MainPage extends DefaultPage {
 
         super.listenLookButton()
     }
-
-    
-    
 }
-
