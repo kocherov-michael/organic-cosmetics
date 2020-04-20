17e20997593dfafc264e9477d11e4da30a01d6ed
Michael <ko4erov@mail.ru>
2020-04-20 15:13:54 +0300

    add sort to main page

--- src/js/MainPage.js
+++ src/js/MainPage.js
@@ -1,4 +1,3 @@
-import {goodsArr} from './goods.js'
 import DefaultPage from './DefaultPage.js'
 
 export default class MainPage extends DefaultPage {
@@ -9,25 +8,53 @@ export default class MainPage extends DefaultPage {
         super.listenAddCartButton()
         super.quantityInput()
         super.listenClosePupUp()
-        console.log(this)
+        // console.log(this)
+        this.listenSortButtons()
     }
     
     // заполняем карточками с товарами главную страницу
-    fillMainPage() {
-        const goodsGrinWrapperElement = document.querySelector('[data-main-goods-grid]')
-        goodsGrinWrapperElement.innerHTML = ''
-        let innerElement = ''
+    fillMainPage(sort=true) {
+      const goodsGrinWrapperElement = document.querySelector('[data-main-goods-grid]')
+      goodsGrinWrapperElement.innerHTML = ''
+      let innerElement = ''
 
-        // const arr = this.goodsArr
-        // ограничиваю вывод на главную страницу только 12 элементами
-        for ( let i = 0; i < 12; i++ ) {
+      let i = 0
+      let setItem = 0
+      // устанавливаем, сколько товаров отображать на странице
+      while(setItem < 12) {
+        if (sort === true) {
+          // добавляем товары все подряд
           innerElement += this.getGoodsTemplate(this.goodsArr[i])
+          setItem++
         }
+        else if (this.goodsArr[i][sort]) {
+          // если товар удовлетворяет условиям сортировки 
+          innerElement += this.getGoodsTemplate(this.goodsArr[i])
+          setItem++
+        } 
+        else if (i >= this.goodsArr.length ) {
+          // если товаров больше нет, а цикл не закончился
+          break
+        }
+        i++
+      }
 
-        goodsGrinWrapperElement.innerHTML = innerElement
-        // показать большую карточку товара
-        super.listenLookButton()
-        // добавить товар в корзину
-        super.listenIconAddToCart()
+      goodsGrinWrapperElement.innerHTML = innerElement
+      // показать большую карточку товара
+      super.listenLookButton()
+      // добавить товар в корзину
+      super.listenIconAddToCart()
+    }
+
+    // прослушка кнопок сортироваки
+    listenSortButtons() {
+      const sortButtonList = document.querySelectorAll('[data-main-sort]')
+      sortButtonList.forEach((button) => {
+        button.addEventListener('click', () => {
+          const sort = button.getAttribute('data-main-sort')
+          // заполняем страницу товарами выбранного сорта
+          this.fillMainPage(sort)
+        })
+      })
     }
 }
