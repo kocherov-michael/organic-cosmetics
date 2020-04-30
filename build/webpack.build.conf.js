90b16aa6590cba919b67a499a91f358c7c504475
Michael <ko4erov@mail.ru>
2020-04-30 17:54:25 +0300

    fix title pug and add CleanWebpackPlugin to webpack

--- build/webpack.build.conf.js
+++ build/webpack.build.conf.js
@@ -1,12 +1,14 @@
 const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
   // BUILD config
   mode: 'production',
   plugins: [
     // если не заданы параметры, то удаляет все файлы из path: path.join(__dirname, '/dist')
-		// new CleanWebpackPlugin()
+    // удаляем при постройке новых файлов. маст хэв! не забыть про подключение в шапке
+		new CleanWebpackPlugin()
   ]
 })
 
